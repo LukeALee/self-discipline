2095ce930daaed17257a726048dacd0accfd7fe8
VoidBug <1576441757@qq.com>
2019-05-12 14:23:40 +0800

    2019-05-12 14:20

---
+++ LatexDoc/codes/exponentialDecayExample.py
@@ -0,0 +1,4 @@
+global_step=tf.Variable(0,trainable=false)
+initial_learning_rate= 0.2
+learning_rate=tf.train.ecponential_decay(initial_learning_rate,global_step,decay_steps=100000,decay_rate=0.95)
+'''pass this learning rate to optimizer as before'''
